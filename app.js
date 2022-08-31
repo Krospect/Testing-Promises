4b1959704c475dfaf94d8e1107ab173f75f004a8
Wahab Dar <wahab14dar@gmail.com>
2022-08-31 07:15:17 -0400

    Removed unnecessary comments

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 // 1. Then
 
-// const emailRef=document.querySelector(".email");
+ const emailRef=document.querySelector(".email");
 
-// fetch("https://jsonplaceholder.typicode.com/users/1")
-// .then(response =>{
-//     return response.json()    
-// }).then(data=>{
-//     console.log(data)
-//     emailRef.innerHTML=data.email
-// })
-
-
-// // 2. Async/Await
-// async function main(){
-//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
-//     const data = await response.json()
-//     console.log(data)
-//     emailRef.innerHTML=data.email
-// }
+ fetch("https://jsonplaceholder.typicode.com/users/1")
+ .then(response =>{
+     return response.json()    
+ }).then(data=>{
+     console.log(data)
+     emailRef.innerHTML=data.email
+ })
 
 
-// main();
+// 2. Async/Await
+async function main(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+    const data = await response.json()
+    console.log(data)
+    emailRef.innerHTML=data.email
+}
 
-// function getSubscriptionStatus(){
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() =>{
-//             resolve("VIP")
-//         },2000);
-//     })
-// }
 
-// //1. Then
-// getSubscriptionStatus().then(response => console.log(response))
+main();
 
-// // 2. Async/Await
-// async function main(){
-// console.log(await getSubscriptionStatus())
-// }
+function getSubscriptionStatus(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() =>{
+            resolve("VIP")
+        },2000);
+    })
+}
 
-// main();
+//1. Then
+getSubscriptionStatus().then(response => console.log(response))
+
+// 2. Async/Await
+async function main(){
+console.log(await getSubscriptionStatus())
+}
+
+main();
 
 
 function getVideo(subscriptionStatus){
